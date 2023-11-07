84432ff8ee6d073bd95a9ee940cf6de06b9196c9
imkdw <imkdw@kakao.com>
2023-11-07 22:13:46 +0900

    chore: prettier, eslint 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+export default class AppController {
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'ok';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import AppController from './app.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export default class AppModule {}
