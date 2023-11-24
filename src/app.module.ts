7402188d424f8f2f2fa5d38f5dc3ad7bd09854a3
imkdw <imkdw@kakao.com>
2023-11-24 23:35:57 +0900

    feat: 트랜스폼 인터셉터, PRISMA 초기설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import AppController from './app.controller';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import TransformInterceptor from './common/interceptos/transform.interceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useValue: TransformInterceptor,
+    },
+  ],
 })
 export default class AppModule {}
