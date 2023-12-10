27a77a4a8d95f29e5019660a4521cc1e751d4030
imkdw <imkdw@kakao.com>
2023-12-10 17:26:10 +0900

    style: 폴더 구조 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import AppController from './app.controller';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import TransformInterceptor from './common/interceptos/transform.interceptor';
-import HealthModule from './modules/health/health.module';
+import HealthModule from './modules/external/health/health.module';
+import TransformInterceptor from './interceptors/transform.interceptor';
 
 @Module({
   imports: [HealthModule],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
