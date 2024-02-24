6da1f72b6fd5e183a9ef6ee6f37242cb5b8bd686
imkdw <imkdw@kakao.com>
2024-02-24 23:55:40 +0900

    Feat: 전역 에러처리 필터 추가

---
+++ src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,24 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { ResponseSuccessType, ResponseType } from '../interceptors/transform.interceptor';
+
+@Catch()
+export default class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const errorMessage = exception instanceof HttpException ? exception.message : 'Internal server error';
+
+    const responseBody: ResponseType = {
+      status: ResponseSuccessType.error,
+      error: errorMessage,
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
