9067b03845e7f6ab3ae37b220aa22b37ad7d969e
imkdw <imkdw@kakao.com>
2024-04-06 22:25:17 +0900

    remove: 미사용 에러코드 400001 제거

--- src/common/exceptions/400.ts
+++ src/common/exceptions/400.ts
@@ -1,17 +1,13 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+// eslint-disable-next-line import/prefer-default-export
 export const BAD_REQUEST_EXCEPTION_CODES = {
   INVALID_ARTICLE_ID: '400001',
 } as const;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class BadRequestException extends HttpException {
   constructor(errorCode: string, data?: string) {
     super({ errorCode, data }, HttpStatus.BAD_REQUEST);
-  }
-}
-
-export class InvalidArticleIdException extends BadRequestException {
-  constructor(data?: string) {
-    super(BAD_REQUEST_EXCEPTION_CODES.INVALID_ARTICLE_ID, data);
   }
 }
