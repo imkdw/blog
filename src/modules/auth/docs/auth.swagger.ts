ff16fb175d2a95ac1104f552dcfd455993384cfe
imkdw <imkdw@kakao.com>
2024-04-27 21:56:56 +0900

    rename: service -> services 폴더명 변경

--- src/modules/auth/docs/auth.swagger.ts
+++ src/modules/auth/docs/auth.swagger.ts
@@ -1,5 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ResponseRefreshTokenDto } from '../dto/response/auth.dto';
 
 // eslint-disable-next-line import/prefer-default-export
-export const refreshToken = (summary: string) => applyDecorators(ApiOperation({ summary }));
+export const refreshToken = (summary: string) =>
+  applyDecorators(ApiOperation({ summary }), ApiCreatedResponse({ type: ResponseRefreshTokenDto }));
