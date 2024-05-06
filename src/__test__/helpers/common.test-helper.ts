4d472bdec1991d5225b5e3455c73c87290f7dde5
imkdw <imkdw@kakao.com>
2024-05-06 20:44:20 +0900

    fix: 게시글 상세정보 조회시 버그 수정

--- src/__test__/helpers/common.test-helper.ts
+++ src/__test__/helpers/common.test-helper.ts
@@ -1,14 +1,12 @@
-import prisma from '../../../prisma/__test__/prisma';
+// // eslint-disable-next-line import/prefer-default-export
+// export const clearDatabase = async () => {
+//   const tables: { TABLE_NAME: string }[] =
+//     await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'imkdw_dev';`;
 
-// eslint-disable-next-line import/prefer-default-export
-export const clearDatabase = async () => {
-  const tables: { TABLE_NAME: string }[] =
-    await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'imkdw_dev';`;
-
-  await prisma.$transaction(async (tx) => {
-    await tx.$executeRaw`SET foreign_key_checks = 0;`;
-    const promises = tables.map((table) => tx.$executeRawUnsafe(`DELETE FROM ${table.TABLE_NAME};`));
-    await Promise.all(promises);
-    await tx.$executeRaw`SET foreign_key_checks = 1;`;
-  });
-};
+//   await prisma.$transaction(async (tx) => {
+//     await tx.$executeRaw`SET foreign_key_checks = 0;`;
+//     const promises = tables.map((table) => tx.$executeRawUnsafe(`DELETE FROM ${table.TABLE_NAME};`));
+//     await Promise.all(promises);
+//     await tx.$executeRaw`SET foreign_key_checks = 1;`;
+//   });
+// };
